a9a14b8fc5e1ea117ba3d394e1bc0e0bb5f9ee5d
Marcin Bojara <marcin_5@mail.ru>
2025-06-03 21:27:04 +0200

    Add API integration for user authentication and profile retrieval
    - Add `gloo-net`, `serde`, and `serde_json` dependencies for HTTP requests and JSON handling.
    - Create `api` module with `user` submodule for API functions: `api_login` and `api_me`.
    - Define `LoginResponse` and `MeResponse` structs for deserialization of API responses.
    - Include `API_URL` constant for base URL configuration in `api` module.
    - Update `Cargo.toml` and `Cargo.lock` to include new dependencies.

---
+++ src/api/mod.rs
@@ -0,0 +1,3 @@
+pub mod user;
+
+pub static API_URL: &'static str = "http://localhost:8000";
---
+++ src/api/user.rs
@@ -0,0 +1,33 @@
+use crate::api::API_URL;
+use gloo_net::http::Request;
+use gloo_net::Error;
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Deserialize)]
+pub struct LoginResponse {
+    pub token: String,
+}
+
+#[derive(Deserialize)]
+pub struct MeResponse {
+    pub id: i32,
+    pub username: String,
+    pub created_at: String,
+}
+
+pub async fn api_login(username: String, password: String) -> Result<LoginResponse, Error> {
+    let response = Request::post(&format!("{}/login", API_URL))
+        .json(&json!({ "username": username, "password":password }))?
+        .send()
+        .await?;
+    response.json::<LoginResponse>().await
+}
+
+pub async fn api_me(token: &String) -> Result<MeResponse, Error> {
+    let response = Request::get(&format!("{}/me", API_URL))
+        .header("Authorization", &format!("Bearer {}", token))
+        .send()
+        .await?;
+    response.json::<MeResponse>().await
+}
