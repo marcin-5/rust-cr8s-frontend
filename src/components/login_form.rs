6a28bfff474b7d45012dd0e4cc97e55fa9a4f83c
Marcin Bojara <marcin_5@mail.ru>
2025-06-03 21:27:55 +0200

    Integrate error handling and API-driven authentication in `LoginForm`
    - Display server error messages using the `Alert` component.
    - Introduce `login` async function for user authentication and fetching profile data.
    - Enhance form submission with error management and response logging.
    - Add `display_error` helper to parse and format error types.

--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -1,6 +1,9 @@
+use crate::api::user::{api_login, api_me, LoginResponse, MeResponse};
+use crate::components::alert::*;
 use crate::components::input::*;
 use gloo_console::log;
 use web_sys::HtmlInputElement;
+use yew::platform::spawn_local;
 use yew::prelude::*;
 
 #[derive(Default, Clone)]
@@ -9,9 +12,28 @@ struct LoginCredentials {
     password: String,
 }
 
+fn display_error(err: &gloo_net::Error) -> String {
+    match err {
+        gloo_net::Error::JsError(e) => format!("Network error: {}", e),
+        gloo_net::Error::SerdeError(e) => format!("Data parsing error: {}", e),
+        _ => "Unknown error occurred".to_string(),
+    }
+}
+
+async fn login(
+    username: String,
+    password: String,
+) -> Result<(LoginResponse, MeResponse), gloo_net::Error> {
+    let login_response = api_login(username, password).await?;
+    let me_response = api_me(&login_response.token).await?;
+    Ok((login_response, me_response))
+}
+
 #[function_component(LoginForm)]
 pub fn login_form() -> Html {
     let credentials = use_state(LoginCredentials::default);
+    let error_message_handle = use_state(String::default);
+    let error_message = (*error_message_handle).clone();
 
     // Create a generic input change handler function
     let create_input_handler =
@@ -40,14 +62,24 @@ pub fn login_form() -> Html {
 
     // Handle form submission
     let form_credentials = credentials.clone();
+    let cloned_error_handle = error_message_handle.clone();
     let onsubmit = Callback::from(move |e: SubmitEvent| {
         e.prevent_default();
         let creds = (*form_credentials).clone();
-        log!("Submitting form", creds.username, creds.password);
+        let error_handle = cloned_error_handle.clone();
+        spawn_local(async move {
+            match login(creds.username, creds.password).await {
+                Ok(responses) => log!(responses.1.username),
+                Err(e) => error_handle.set(display_error(&e)),
+            }
+        });
     });
 
     html! {
         <form onsubmit={onsubmit}>
+            if !error_message.is_empty() {
+                <Alert alert_type={"danger"} message={error_message} />
+            }
             <div class="mb-3">
                 <Input
                     input_type="text"
