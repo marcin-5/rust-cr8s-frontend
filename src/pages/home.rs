6f67abf4649a8c667fd22e1ba86da2072d8fcd3b
Marcin Bojara <marcin_5@mail.ru>
2025-06-06 16:26:54 +0200

    Update `Home` to display user-specific welcome message and redirect unauthenticated users to `Login`

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,10 +1,21 @@
+use crate::{contexts::CurrentUserContext, Route};
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
-    html! {
-        <div class="container">
-            {"Welcome user..."}
-        </div>
+    let current_user_ctx =
+        use_context::<CurrentUserContext>().expect("Current user context is missing");
+    match &current_user_ctx.user {
+        Some(user) => {
+            html! {
+                <div class="container text-center">
+                    {"Welcome user "}{user.username.clone()}
+                </div>
+            }
+        }
+        None => html! {
+            <Redirect<Route> to={Route::Login} />
+        },
     }
 }
