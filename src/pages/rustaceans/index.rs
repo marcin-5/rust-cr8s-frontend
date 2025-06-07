3eafec27f46520317e50ef1960580dae3200d2c9
Marcin Bojara <marcin_5@mail.ru>
2025-06-07 16:41:54 +0200

    Add `Rustaceans` page with layout and components
    - Introduce `Rustaceans` page under `src/pages/rustaceans`.
    - Add `Header`, `Sidebar`, and `RustaceanList` components to the page layout.
    - Update `mod.rs` to include the new `rustaceans` module.

---
+++ src/pages/rustaceans/index.rs
@@ -0,0 +1,22 @@
+use yew::prelude::*;
+
+use crate::components::header::Header;
+use crate::components::rustacean_list::RustaceanList;
+use crate::components::sidebar::Sidebar;
+
+#[function_component(Rustaceans)]
+pub fn rustaceans() -> Html {
+    html! {
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-auto">
+                    <Sidebar />
+                </div>
+                <div class="col mt-3">
+                    <Header />
+                    <RustaceanList />
+                </div>
+            </div>
+        </div>
+    }
+}
