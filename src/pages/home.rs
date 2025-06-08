7393a56857aaa9c7275d6e58f6cda4775730545a
Marcin Bojara <marcin_5@mail.ru>
2025-06-08 12:00:06 +0200

    Refactor `Home` page to use `AuthenticatedLayout` component
    - Replace `Header` and `Sidebar` components with `AuthenticatedLayout`.

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,20 +1,11 @@
-use crate::components::header::Header;
-use crate::components::sidebar::Sidebar;
+use crate::pages::common::layout::AuthenticatedLayout;
 use yew::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-auto">
-                    <Sidebar />
-                </div>
-                <div class="col mt-3">
-                    <Header />
-                    {"Have a great day!"}
-                </div>
-            </div>
-        </div>
+        <AuthenticatedLayout>
+            {"Have a great day!"}
+        </AuthenticatedLayout>
     }
 }
